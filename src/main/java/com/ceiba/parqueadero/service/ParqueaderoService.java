42dfe1ea9357832bf0e6a23baa4f4ec368b15296
Alejolaya <alejolaya@gmail.com>
2019-04-19 15:23:25 -0500

    quitar las interfaces que sobraban

--- src/main/java/com/ceiba/parqueadero/service/ParqueaderoService.java
+++ src/main/java/com/ceiba/parqueadero/service/ParqueaderoService.java
@@ -1,46 +1,75 @@
-package com.ceiba.parqueadero.service;
+package com.ceiba.parqueadero.service.imp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ceiba.parqueadero.model.Parqueadero;
+import com.ceiba.parqueadero.model.Vehiculo;
+import com.ceiba.parqueadero.repository.ParqueaderoRepository;
+import com.ceiba.parqueadero.service.ParqueaderoService;
+import com.ceiba.parqueadero.service.VehiculoService;
 
-public interface ParqueaderoService {
+@Service
+public class ParqueaderoServiceImp implements ParqueaderoService {
 
-	/**
-	 * Guarda el parqueadero
-	 * @param parqueadero
-	 * @return
-	 */
-	Parqueadero save(Parqueadero parqueadero);
+	@Autowired
+	protected ParqueaderoRepository parqueaderoRespository;
 	
+	@Autowired
+	protected VehiculoService vehiculoService;
 	
-	/**
-	 * Borra el parqueadero
-	 * @param id
-	 */
-	void deleteParqueaderoById(Long id);
-	
-	/**
-	 * Encontrar el parqueadero
-	 * @param id
-	 * @return
-	 */
-	Parqueadero findById(Long id);
-	
-	/**
-	 * Disminuye la cantidad de celdas disponibles de carro y retorna la cantidad restante
-	 * @return
-	 */
-	Integer disminuirCeldaMotoDisponible(Long id);
-	/**
-	 * Disminuye la cantidad de celdas disponibles de moto y retorna la cantidad restante
-	 * @return
-	 */
-	Integer disminuirCeldaCarroDisponible(Long id);
+	@Override
+	public Parqueadero save(Parqueadero parqueadero) {
+		
+		return parqueaderoRespository.save(parqueadero);
+	}
 
-/**
- * libera la celda donde se encuentre el vehiculo con la respectiva placa
- * @param placa
- */
-	void liberarCelda(String placa);
+	@Override
+	public void deleteParqueaderoById(Long id) {
+		parqueaderoRespository.delete(id);
+
+	}
+
+	@Override
+	public Parqueadero findById(Long id) {
+		
+		return parqueaderoRespository.findOne(id);
+	}
+
+	@Override
+	public Integer disminuirCeldaMotoDisponible(Long id) {
+		Parqueadero parqueadero = parqueaderoRespository.findOne(id);
+		parqueadero.setCeldasMoto(parqueadero.getCeldasMoto()-1);
+		parqueadero = parqueaderoRespository.save(parqueadero);
+		return parqueadero.getCeldasMoto();
+	}
+
+	@Override
+	public Integer disminuirCeldaCarroDisponible(Long id) {
+		Parqueadero parqueadero = parqueaderoRespository.findOne(id);
+		parqueadero.setCeldasCarro(parqueadero.getCeldasCarro()-1);
+		parqueadero = parqueaderoRespository.save(parqueadero);
+		return parqueadero.getCeldasCarro();
+	}
+
+	@Override
+	public void liberarCelda(String placa) {
+		
+		Vehiculo vehiculo =new Vehiculo();
+		vehiculo.setPlaca(placa);
+		vehiculo = vehiculoService.findByPlaca(vehiculo);
+		
+		Parqueadero parqueadero = parqueaderoRespository.findOne(1L);
+		
+		if ("M".equalsIgnoreCase(vehiculo.getTipoVehiculo())) {
+			parqueadero.setCeldasMoto(parqueadero.getCeldasMoto()+1);
+		}else {
+			parqueadero.setCeldasMoto(parqueadero.getCeldasCarro()+1);
+		}
+		
+		parqueaderoRespository.save(parqueadero);
+	}
+
 	
-	
+
 }
--- src/main/java/com/ceiba/parqueadero/service/ParqueoService.java
+++ src/main/java/com/ceiba/parqueadero/service/ParqueoService.java
@@ -1,94 +1,181 @@
-package com.ceiba.parqueadero.service;
+package com.ceiba.parqueadero.service.imp;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ceiba.parqueadero.model.Parqueadero;
 import com.ceiba.parqueadero.model.Parqueo;
 import com.ceiba.parqueadero.model.Vehiculo;
+import com.ceiba.parqueadero.repository.ParqueoRepository;
+import com.ceiba.parqueadero.service.ParqueaderoService;
+import com.ceiba.parqueadero.service.ParqueoService;
+import com.ceiba.parqueadero.service.VehiculoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-public interface ParqueoService {
-	/**
-	 * Guardar un parqueo
-	 * 
-	 * @param parqueo
-	 * @return
-	 */
-	Parqueo save(Parqueo parqueo);
+@Service
+public class ParqueoServiceImp implements ParqueoService {
 
-	/**
-	 * Guarda un parqueo y le adiciona la fecha de ingreso
-	 * 
-	 * @param parqueo
-	 * @return
-	 */
-	Parqueo ingresar(Parqueo parqueo);
+	@Autowired
+	protected ParqueoRepository parqueoRepository;
 
-	/**
-	 * Busca el vehiculo y el parqueadero, hace el cobro y guarda
-	 * 
-	 * @param parqueo
-	 * @return
-	 */
-	Parqueo pagar(Parqueo parqueo);
+	@Autowired
+	protected VehiculoService vehiculoService;
 
-	/**
-	 * Busca el parque por placa
-	 * 
-	 * @param placa
-	 * @return
-	 */
-	Parqueo findByPlaca(String placa);
-
-	/**
-	 * Busca parque por placa y que no tengan fecha de salida
-	 * 
-	 * @param placa
-	 * @param fechaSalida
-	 * @return
-	 */
-	Parqueo findByPlacaAndFechaSalida(String placa, Date fechaSalida);
-
-	/**
-	 * Calcula la cantidad de horas a pagar
-	 * 
-	 * @param difEnHoras
-	 * @param difEnMinutos
-	 * @return
-	 */
-	Long calcularHorasAPagar(Long difEnHoras, Long difEnMinutos);
-
-	/**
-	 * Calcula la cantidad de Dias a pagar
-	 * 
-	 * @param difEnHoras
-	 * @return
-	 */
-	Long calcularDiasAPagar(Long difEnHoras);
-
-	/**
-	 * Hace el calculo de la cantidad de dinero total que debe pagar
-	 * 
-	 * @param vehiculo
-	 * @param parqueadero
-	 * @param diasAPagar
-	 * @param horasRestantes
-	 * @return
-	 */
-	Long calcularTotalApagar(Vehiculo vehiculo, Parqueadero parqueadero, Long diasAPagar, Long horasRestantes);
+	@Autowired
+	protected ParqueaderoService parqueaderoService;
 	
-	/**
-	 * Convierte Json a parqueo y el campo placa lo convierte a mayuscula
-	 * @param parqueoJson
-	 * @return
-	 * @throws Exception
-	 */
-	Parqueo convertirJsonAParqueo(String parqueoJson) throws Exception;
+	protected ObjectMapper mapper;
+
+	@Override
+	public Parqueo ingresar(Parqueo parqueo) {
+
+		// obtener tiempo actual
+		Date fechaActual = new Date();
+
+		// crear el objeto a insertar la db
+		parqueo.setFechaIngreso(fechaActual);
+		parqueo.setParqueadero(1L);
+		// insertarlo en la base de datos
+
+		return parqueoRepository.save(parqueo);
+	}
+
+	@Override
+	public Parqueo pagar(Parqueo parqueo) {
+		// obtener tiempo actual
+		// calcular numero de horas
+		// consultar vehiculo
+		// validar si es moto de alto cc
+		//
+
+		Vehiculo vehiculo = new Vehiculo();
+		vehiculo.setPlaca(parqueo.getPlaca());
+		vehiculo = vehiculoService.findByPlaca(vehiculo);
+		Parqueadero parqueadero = parqueaderoService.findById(1L);
+
+		// Calcular diferencia de fechas
+		Date fechaActual = new Date();
+		Date fechaIngreso = parqueo.getFechaIngreso();
+		Long diffInMillies = calcularDiferenciaEnMiliSeg(fechaActual, fechaIngreso);
+		Long difEnHoras = TimeUnit.HOURS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+		Long difEnMinutos = TimeUnit.MINUTES.convert(diffInMillies, TimeUnit.MILLISECONDS);
+		DecimalFormat df = new DecimalFormat("##");
+		df.setRoundingMode(RoundingMode.DOWN);
+
+		Long diasAPagar = calcularDiasAPagar(difEnHoras);
+
+		Long horasRestantes = calcularHorasAPagar(difEnHoras, difEnMinutos);
+
+		Long totalAPagar = calcularTotalApagar(vehiculo, parqueadero, diasAPagar, horasRestantes);
+
+		parqueo.setFechaSalida(fechaActual);
+		parqueo.setCosto(Integer.valueOf(df.format(totalAPagar)));
+
+		return parqueo;
+	}
+
+	@Override
+	public Long calcularDiferenciaEnMiliSeg(Date fechaActual, Date fechaIngreso) {
+		return fechaActual.getTime() - fechaIngreso.getTime();
+		
+	}
 	
-	/**
-	 * Calcular la diferencia en milisegundos entre la fecha de ingreso y la de salida
-	 * @param fechaActual
-	 * @param fechaIngreso
-	 * @return
-	 */
-	Long calcularDiferenciaEnMiliSeg(Date fechaActual, Date fechaIngreso);
+	@Override
+	public Long calcularDiasAPagar(Long difEnHoras) {
+		DecimalFormat df = new DecimalFormat("##");
+		
+		Long diasAPagar = Long.valueOf(df.format(difEnHoras / 24));
+		
+		if (difEnHoras % 24 >= 9) {
+			diasAPagar++;
+			
+		}
+		return diasAPagar;
+	}
+	
+	@Override
+	public Long calcularHorasAPagar(Long difEnHoras, Long difEnMinutos) {
+		DecimalFormat df = new DecimalFormat("##");
+		Long horasRestantes = difEnHoras % 24;
+		if (horasRestantes >= 9) {
+			horasRestantes = 0L;
+		} else {
+			df.setRoundingMode(RoundingMode.UP);
+			horasRestantes = Long.valueOf(df.format(horasRestantes));
+		}
+
+		if (difEnMinutos < 60) {
+			horasRestantes++;
+		} else {
+			Long minutosRestantes = difEnMinutos % 60;
+			if (minutosRestantes > 0) {
+				horasRestantes++;
+			}
+		}
+		return horasRestantes;
+	}
+
+	@Override
+	public Long calcularTotalApagar(Vehiculo vehiculo, Parqueadero parqueadero, Long diasAPagar, Long horasRestantes) {
+		Long horasAPagar = horasRestantes;
+		Long totalAPagar;
+		if ("M".equalsIgnoreCase(vehiculo.getTipoVehiculo())) {
+			if (vehiculo.getCilindraje() >= 500) {
+				totalAPagar = diasAPagar * parqueadero.getMotoDia() + horasAPagar * parqueadero.getMotoHora()
+						+ parqueadero.getAltoCc();
+			} else {
+				totalAPagar = diasAPagar * parqueadero.getMotoDia() + horasAPagar * parqueadero.getMotoHora();
+			}
+
+		} else {
+			totalAPagar = diasAPagar * parqueadero.getCarroDia() + horasAPagar * parqueadero.getCarroHora();
+		}
+		return totalAPagar;
+	}
+
+	@Override
+	public Parqueo findByPlaca(String placa) {
+
+		return parqueoRepository.findByPlaca(placa);
+	}
+
+	@Override
+	public Parqueo findByPlacaAndFechaSalida(String placa, Date fechaSalida) {
+
+		return parqueoRepository.findByPlacaAndFechaSalida(placa, fechaSalida);
+	}
+
+	@Override
+	public Parqueo save(Parqueo parqueo) {
+
+		return parqueoRepository.save(parqueo);
+	}
+
+	@Override
+	public Parqueo convertirJsonAParqueo(String parqueoJson) throws Exception {
+		this.mapper = new ObjectMapper();
+		
+		Parqueo parqueo = this.mapper.readValue(parqueoJson, Parqueo.class);
+		//remover espacios
+		String placa = parqueo.getPlaca().trim();
+		placa=placa.replaceAll("//s", "");
+		//Convertir la placa a mayuscula
+		
+		placa = placa.toUpperCase();
+		parqueo.setPlaca(placa);
+		//convertir placa a mayuscula
+		placa = placa.toUpperCase();
+		parqueo.setPlaca(placa);
+		
+		
+		return parqueo;
+	}
+	
+	
+
 }
